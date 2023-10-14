9022c34e745743ac507e7e96ab7502765b8d7576
BhawanaFarswan <140094847+BhawanaFarswan@users.noreply.github.com>
2023-10-14 11:43:56 +0530

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+
+function showSlides() {
+    const slides = document.querySelectorAll('.slide');
+    const prev = document.querySelector('.prev');
+    const next = document.querySelector('.next');
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = 'block';
+
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+showSlides();
